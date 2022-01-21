b4b133f6c2006fa71f6d91b0e02398cddb358e0d
Bongani <bearnomangola7@gmail.com>
2022-01-21 11:25:21 +0200

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+        <h1>{{ title }}</h1>
+        <p>Have a look at our promotion</p>
+        <teleport to=".modals" v-if="showModal">
+    <Modal theme= "promotion" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Register</a>
+        <a href="#">Find Out More</a>
+      </template>
+      <h1>Bongani</h1>
+      <p>Come get your 50% discount now whilst stock last!</p>
+      </Modal>
+        </teleport>
+
+        <teleport to=".modals" v-if="showModalTwo">
+          <Modal @close="toggleModalTwo">
+            <h1>Register for more promotions</h1>
+            <p>Do not miss out of upcoming promo codes!</p>
+            </Modal>
+        </teleport>
+        <button @click="toggleModal">Info</button>
+        &nbsp;
+        <button @click="toggleModalTwo">Register</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+ 
+  data() {
+    return {
+      title: 'Bongani',
+      showModal: false,
+      showModalTwo: false
+    }
+  },
+
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    }
   }
+
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{promotion: theme === 'promotion'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['header', 'text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    },
+}
+</script>
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: darkolivegreen;
+    border-radius: 10px;
+}
+
+.modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+}
+
+.modal.promotion {
+    background: crimson;
+    color: white;
+}
+
+.modal.promotion h1 {
+    color: white;
+}
+
+.modal.promotion .actions {
+    color: white;
+}
+
+.modal.promotion .actions a {
+    color:  white;
+}
+
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+
+h1 {
+    color: black;
+    border: none;
+    padding: 0;
+}
+
+p {
+    color: white;
+}
+
+</style>
